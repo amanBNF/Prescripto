2b4fce33fb85c4aaea7d18c3c4f8baa0d7e99a42
Aman Shukla <aman29shukla@gmail.com>
2025-09-28 20:41:17 +0530

    Prescripto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Doctors from './pages/Doctors'
+import Appointments from './pages/Appointments'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div id='root' className='mx-4 sm:mx-[10%]'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/appointments/:docId' element={<Appointments />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
