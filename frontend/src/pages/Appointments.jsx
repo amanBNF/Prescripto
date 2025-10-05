4909ee44b82c030d77b2ed18c1d035058e40f55d
Aman Shukla <aman29shukla@gmail.com>
2025-10-05 16:51:49 +0530

    days and date part of UI

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -9,16 +9,64 @@ const Appointments = () => {
   const { doctors, CurrencySymbol } = useContext(AppContext);
 
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlot, setDocSlot] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDoctor = async () => {
     const docInfo = doctors.find(doc => doc._id === docId);
     setDocInfo(docInfo);
-    console.log(docInfo);
+  }
+
+  const getAvailableSlots = async () => {
+    setDocSlot([]);
+
+    let todayDate = new Date();
+
+    for(let i=0; i<7; i++){
+      let currentDate = new Date(todayDate);
+      currentDate.setDate(todayDate.getDate() + i);
+
+      let endTime = new Date();
+      endTime.setDate(todayDate.getDate() + i);
+      endTime.setHours(21,0,0,0);
+
+      if(todayDate.getDate() === currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10);
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      }else{
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = []
+
+      while(currentDate < endTime){
+        let formattedTime = currentDate.toLocaleTimeString([], {hour : '2-digit', minute : '2-digit'})
+        
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+
+      setDocSlot(prev => ([...prev, timeSlots]))
+    }
   }
 
   useEffect(() => {
     fetchDoctor();
   }, [doctors, docId])
+
+  useEffect(() => {
+    getAvailableSlots(); 
+  },[docInfo])
+
+  useEffect(() => {
+    console.log(docSlot )
+  },[docSlot])
 
   if (!docInfo) {
     return <p>loading........</p>
@@ -49,8 +97,8 @@ const Appointments = () => {
             <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
           </div>
 
-          <p>
-            Appointment Fee : <span>{CurrencySymbol}{docInfo.fees}</span>
+          <p className='text-gray-500 font-medium mt-4'>
+            Appointment Fee : <span className='text-gray-800'>{CurrencySymbol}{docInfo.fees}</span>
           </p>
 
         </div>
