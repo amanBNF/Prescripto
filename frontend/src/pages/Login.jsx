c27e428b9f73a24af8225ee9f77634c283b18682
Aman Shukla <aman29shukla@gmail.com>
2025-10-07 12:10:50 +0530

    Login page and profile page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,26 +16,31 @@ const Login = () => {
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
         <p>Please {state === 'Sign Up' ? "sign up" : "login"} to book appointment</p>
 
-        <div className='w-full'>
-          <p>Full Name</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(event) => setName(email.target.name)} value={name} required />
-        </div>
+        {
+          state === 'Sign Up' &&
+            <div className='w-full'>
+              <p>Full Name</p>
+              <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(event) => setName(email.target.name)} value={name} required />
+            </div>
+        }
+
+
 
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1'  type="email" onChange={(event) => setName(email.target.email)} value={email} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(event) => setName(email.target.email)} value={email} required />
         </div>
 
         <div className='w-full'>
           <p>Password</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1'  type="password" onChange={(event) => setName(email.target.password)} value={password} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(event) => setName(email.target.password)} value={password} required />
         </div>
 
         <button className='bg-primary text-white w-full py-2 rounded-md text-base cursor-pointer'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
         {
-          state === 'Sign Up' ? 
-          <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p> :
-          <p>Create an new account? <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
+          state === 'Sign Up' ?
+            <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p> :
+            <p>Create an new account? <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
         }
       </div>
     </form>
