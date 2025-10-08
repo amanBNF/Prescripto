9b218f7464f477d4495bbb3f67d316ded58ad559
Aman Shukla <aman29shukla@gmail.com>
2025-10-08 21:26:13 +0530

    backend creation

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
           <p className='font-semibold text-lg text-gray-600'>Careers at PRESCRIPTO</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
 
-          <button className='border border-black px-8 py-4 text-sm cursor-pointer hover:bg-black hover:text-white hover:rounded'>Explore Jobs</button>
+          <button className='border border-black px-8 py-4 text-sm cursor-pointer hover:bg-black hover:text-white hover:rounded transition-all duration-300'>Explore Jobs</button>
         </div>
       </div>
     </div>
