6f87e7dd35c4c7371e5e48d6f545a2dc39749344
Aman Shukla <aman29shukla@gmail.com>
2025-10-09 21:45:25 +0530

    UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div>
 
       <div className='text-center text-2xl pt-10 text-gray-500'>
-        <p>About <span className='text-gray-700 font-medium'>US</span></p>
+        <p>ABOUT<span className='text-gray-700 font-medium'>US</span></p>
       </div>
 
       <div className='my-10 flex flex-col md:flex-row gap-12'>
