3aff55f731172ffc501cd46e94d670a3b84e8d82
Aman Shukla <aman29shukla@gmail.com>
2025-10-10 23:04:40 +0530

    New UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
       <div className='flex flex-col md:flex-row mb-20'>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>Efficiency:</b>
-          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle...</p>
         </div>
         <div className='border px-10 md:px-16 py-8 sm:py-16 flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
           <b>Convenience:</b>
