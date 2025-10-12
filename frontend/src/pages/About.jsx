73be74a6d21b953a1de23f35ff16a5a266a2e199
Aman Shukla <aman29shukla@gmail.com>
2025-10-12 15:59:10 +0530

    Update About page to enhance the 'CHOOSE US' section text

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       </div>
 
       <div className='text-xl my-4'>
-        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US !</span></p>
       </div>
 
       <div className='flex flex-col md:flex-row mb-20'>
