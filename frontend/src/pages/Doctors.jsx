b303390da2bc51b8c53e5592534c33c9d511bd99
Aman Shukla <aman29shukla@gmail.com>
2025-10-14 23:06:42 +0530

    .

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -71,7 +71,7 @@ const Doctors = () => {
                 : "bg-white text-gray-700 border-gray-300 hover:bg-gray-100"
               }`}
           >
-            All
+            All Doctors
           </button>
         </div>
 
